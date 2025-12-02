433e885ca70d779c76250d16d1a7195d05a86f9b
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-12-02 20:46:43 +0100

    feat(day2): add input parser

--- day2/repetition-identificator/src/main.rs
+++ day2/repetition-identificator/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+mod parser;
+mod range_preprocessor;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+    let file_path = &args[1];
+    let file_content = std::fs::read_to_string(file_path).expect("Failed to read the file");
+
+    let ranges = parser::parse(&file_content);
 }
+
