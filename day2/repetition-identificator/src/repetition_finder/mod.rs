a53477e1543868089b4982006d292a0ed9ac2f78
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-12-14 00:38:24 +0100

    feat(day2-part2): implement split_length_finder

--- day2/repetition-identificator/src/repetition_finder/mod.rs
+++ day2/repetition-identificator/src/repetition_finder/mod.rs
@@ -50,6 +50,23 @@ impl Restriction {
 }
 
 impl Range {
+    pub fn find_repetitions_part2(&self) -> Vec<u64> {
+        vec![]
+    }
+
+    fn find_split_length(&self) -> Vec<u8> {
+        let lower_bound_digits = vector_of_digits(self.lower_bound);
+        let lower_bound_digits_length = lower_bound_digits.len();
+        let half_length = lower_bound_digits_length / 2;
+
+        (1..=half_length)
+            .filter(|digit_count| {
+                lower_bound_digits_length.rem_euclid(*digit_count) == 0
+            })
+            .map(|digit_count| digit_count as u8)
+            .collect::<Vec<u8>>()
+    }
+
     pub fn find_repetitions(&self) -> Vec<u64> {
         let lower_digits = vector_of_digits(self.lower_bound);
         let upper_digits = vector_of_digits(self.upper_bound);
